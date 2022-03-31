575187ff71cb50cf0b6591d05431ed2a2054cc66
Samuel <Samuel@PC-SAMUEL>
2022-03-31 21:56:37 +0200

    Añado un punto final en la linea 7

--- EntornosUD4Tarea3/src/GitHub1.java
+++ EntornosUD4Tarea3/src/GitHub1.java
@@ -4,7 +4,7 @@ public class GitHub1 {
 	public static void main(String[] args) {
 
 
-		System.out.println("En este proyecto trabajaremos con el repositorio GITHUB");
+		System.out.println("En este proyecto trabajaremos con el repositorio GITHUB.");
 
 	}
 
